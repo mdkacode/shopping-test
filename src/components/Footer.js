33494ef667e2ef051f23a29e6aae6dec3e6627d3
hmm-mayank <mayankanildwivedi@gmail.com>
2020-02-26 13:51:53 +0530

    Adding all to it

---
+++ src/components/Footer.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+const Footer = props => {
+  return (
+    <footer>
+      <p className="footer-links">
+        
+        <span> / </span>
+        <a href="https://github.com/hmm-mayank" target="_blank">
+          Shopping Site
+        </a>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
